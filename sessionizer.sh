70bc34eec22887bdb72fa1eed670706826f8c8c6
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-01-12 17:33:08 +0100

    add sessionizer script

---
+++ sessionizer.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This script is used to create a new tmux session based on the repo directory
+# borrowed from https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=~/repo/$(ls ~/repo | fzf)
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+#trim everything but directory name
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+tmux switch-client -t $selected_name
