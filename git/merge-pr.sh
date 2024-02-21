6881eb0fb5f329ac6aec7f6f4d87030e609688c5
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-02-21 15:00:00 +0100

    rm last 2 prompts

--- git/merge-pr.sh
+++ git/merge-pr.sh
@@ -11,23 +11,9 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 	exit 0
 fi
 
-# remember the current branch
-current_branch=$(git symbolic-ref --short HEAD)
 # get the pull request number
 pr_number=$(gh pr view --json number --jq '.number' | tr -d '\n')
 
 git merge-into "$1" "--no-ff" "-m Merge PR #$pr_number: \"$2\""
 
 lazygit #open log to inspect the merge commit
-
-#ask if the user wants to push the changes
-read -p "Do you want to push the changes? (y/n) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	git push origin "$1"
-fi
-
-# ask if the user wants to delete the branch
-read -p "Do you want to delete the branch? (y/n) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	git delete branch "$current_branch"
-fi
