5bf5f00ae7982a4beaebe932701cc29012e63623
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-04-15 16:09:38 +0200

    update pr script

--- git/merge-pr.sh
+++ git/merge-pr.sh
@@ -11,9 +11,10 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 	exit 0
 fi
 
-# get the pull request number
 pr_number=$(gh pr view --json number --jq '.number' | tr -d '\n')
 
-git merge-into "$1" "--no-ff" "-m Merge PR #$pr_number: \"$2\""
+pr_title=$(gh pr view --json title --jq '.title' | tr -d '\n')
+
+git merge-into "$1" "--no-ff" "-m Merge PR #$pr_number: \"$pr_title\""
 
 lazygit #open log to inspect the merge commit
