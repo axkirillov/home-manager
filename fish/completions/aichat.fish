db356603219bd6b246dd2910089da35d6552e0d5
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-03-06 13:08:53 +0100

    add aichat completions

---
+++ fish/completions/aichat.fish
@@ -0,0 +1,9 @@
+function _aichat_fish
+    set -l _old (commandline)
+    if test -n $_old
+        echo -n "⌛"
+        commandline -f repaint
+        commandline (aichat -e $_old)
+    end
+end
+bind \ea _aichat_fish
