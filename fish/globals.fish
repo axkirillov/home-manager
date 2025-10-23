91b40060e54e9a87546dc03c266e1294b347a1b9
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-10-23 14:13:39 +0200

    chore(env): add nix paths to PATH

--- fish/globals.fish
+++ fish/globals.fish
@@ -5,3 +5,4 @@ set -xg VISUAL $EDITOR
 # needed for ruby integration with neovim
 set -xg GEM_HOME $HOME/.gem/bin/
 set -xg HOMEBREW_NO_AUTO_UPDATE 1
+set -xg PATH /nix/var/nix/profiles/default/bin /nix/var/nix/profiles/default/sbin $PATH
