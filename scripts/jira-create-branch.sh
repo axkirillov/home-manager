27ead632f4d4f8445c6e8c8cc4c12064eeb62c19
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2026-01-15 15:07:53 +0100

    delete unused scripts

--- scripts/jira-create-branch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-jira_title=$(jira issue list -q "key = $1" --plain | awk 'NR==2' | awk -F '\t' '{ print $3 }')
-branchName=$(echo "$jira_title" | sed "s/[^[:alpha:].-]/-/g" | tr '[:upper:]' '[:lower:]')
-
-git checkout -b "$1-$branchName"
-
--- scripts/pr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gh pr list --json "author,number,title" | jq -r '.[] | "\(.number) - \(.title) - \(.author.login)"' | fzf | cut -d' ' -f1 | xargs gh pr checkout
